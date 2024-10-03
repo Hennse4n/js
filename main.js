3976aea20207ad93eee76b6f719d4a1ff016c459
Hennsen Aldana <165119064+Hennse4n@users.noreply.github.com>
2024-10-03 13:57:14 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 const button = document.querySelector(".bttn");
+const save = document.querySelector(".save");
 const text = document.querySelector(".texto");
 
 button.addEventListener('click', function(){
     const list = document.querySelector(".lista");
-    const txt = document.getElementsByName("name")[0].value
+    var txt = document.getElementsByName("name")[0].value
     
-    const new_li = document.createElement("li");
-    new_li.textContent = txt;
+     const new_li = document.createElement("li");
+     new_li.textContent = txt;
 
-    const new_btn = document.createElement("button");
-    new_btn.textContent = "Borrar";
-
+     const new_btn = document.createElement("button");
+     new_btn.textContent = "Borrar";
+     new_btn.className = "del";
     
-    new_btn.addEventListener('click', function(){
+     new_btn.addEventListener('click', function(){
         new_li.remove();
         new_btn.remove();
-    });
+     });
 
-    new_li.appendChild(new_btn);
-    list.appendChild(new_li);
+     new_li.appendChild(new_btn);
+     list.appendChild(new_li);
 });
  
 
+save.addEventListener('click', function(){
+    window.print();
+});
